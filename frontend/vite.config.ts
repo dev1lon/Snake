67ce492386ecf2916e29455244fae71655609831
devilon <normis1337@gmail.com>
2026-05-06 15:44:13 +0300

    Fix SIWE browser buffer polyfill

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      buffer: "buffer/"
+    }
+  },
   server: {
     port: 5173
   }
